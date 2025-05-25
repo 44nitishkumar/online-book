66357b275ef89f102ad5a672488052d431c17d1e
Nitish <44nitishkumar@gmail.com>
2025-05-25 21:15:42 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import {Link} from "react-router"
+
+
+const Navbar = () => {
+    return(
+        <nav>
+            <div>
+                <div>
+                    <Link>PickBazar</Link>
+                </div>
+            </div>
+        </nav>
+    )
+}
+export default Navbar
